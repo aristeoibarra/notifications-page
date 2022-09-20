c5a3d65689360835bd0ffd5ac279a2c7b721d74d
aristeoibarra <aristeoibarra608@gmail.com>
2022-09-20 00:44:07 -0600

    fix: move fonts to assets directory and minor changes

--- src/i18n.js
+++ src/i18n.js
@@ -2,10 +2,13 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(Backend).use(initReactI18next).init({
-  fallbackLng: "en",
-  lng: "en",
-  debug: true,
-});
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    lng: "en",
+    debug: process.env.NODE_ENV === "development",
+  });
 
 export default i18n;
